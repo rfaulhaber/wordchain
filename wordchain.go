2f2e5a1e1f5ebd12bddbf856c4dddadf9e7840b3
rfaulhaber <faulhaber.14@osu.edu>
2016-07-03 16:12:24 -0400

    Wordchain builder started

--- wordchain.go
+++ wordchain.go
@@ -11,22 +11,50 @@ func main() {
 	indexFlagPtr := flag.String("i", "", "Path to a text file containing a list of properly formatted words")
 	flag.Parse()
 	args := os.Args[2:]
+	startWord := args[0]
+	goalWord := args[1]
 
 	fmt.Println(args)
+	fmt.Println("start word: ", startWord, "goal word:", goalWord)
 
 	if *indexFlagPtr == "" {
 		fmt.Println("Please specify a word list!")
+	} else if len(startWord) != len(goalWord) {
+		fmt.Println("Start word and goal word must be of equal length!")
 	} else {
 		root := node.BuildTree(*indexFlagPtr)
 
-		startWord := args[0]
-		goalWord := args[1]
+		currentWord := startWord
 
-		fmt.Println("words: ", startWord, " ", goalWord)
-
-		// tests if HasWord works
-		fmt.Println(root.HasWord("conk"))
-		fmt.Println(root.HasWord("cong"))
-		fmt.Println(root.HasWord("dog"))
+		fmt.Print(startWord)
+		for currentWord != startWord {
+			currentWord = suggestNextWord(currentWord, goalWord, root)
+			fmt.Print(currentWord)
+		}
 	}
+}
+
+func suggestNextWord(currentWord string, goalWord string, root *node.Node) string {
+	thisWord := currentWord
+
+	for !isWordInTree(thisWord, root) {
+		for i, c := range goalWord {
+			if string(thisWord[i]) == string(c) {
+				continue
+			} else {
+				thisWord = stringReplace(thisWord, i, string(c))
+				break
+			}
+		}
+	}
+
+	return thisWord
+}
+
+func isWordInTree(word string, root *node.Node) bool {
+	return root.HasWord(word)
+}
+
+func stringReplace(word string, index int, newChar string) string {
+	return word[:index] + string(newChar) + word[index + 1:]
 }
